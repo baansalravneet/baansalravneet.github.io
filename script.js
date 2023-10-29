574814b5df8bb2edf007a8928dd7694198afc598
baansalravneet <34987414+baansalravneet@users.noreply.github.com>
2023-10-29 17:22:05 -0500

    added TTT

---
+++ script.js
@@ -0,0 +1,29 @@
+setTimeout(function() {
+    const content = document.querySelector('p#main-text');
+    content.style.opacity = 1;
+    content.style.transform = 'translateY(0)';
+}, 500);
+
+setTimeout(function() {
+    const content = document.querySelector('p#secondary-text');
+    content.style.opacity = 1;
+    content.style.transform = 'translateY(0)';
+}, 1500);
+
+setTimeout(function() {
+    const content = document.querySelector('p#last-text');
+    content.style.opacity = 1;
+    content.style.transform = 'translateY(0)';
+}, 2500);
+
+setTimeout(function() {
+    const pages = document.getElementById('pages');
+    pages.style.zIndex = 0;
+}, 3500);
+
+setTimeout(function() {
+    const content = document.getElementById('ttt-board');
+    content.style.opacity = 1;
+    content.style.transform = 'translateY(0)';
+    content.style.pointerEvents = "auto";
+}, 3500);
